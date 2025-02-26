0d165304cb4aaf183fc8a484390e3fe087277eb7
Yash-Gupta001 <gyash4963@gmail.com>
2025-02-26 11:20:35 +0530

    ui improved

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -12,6 +12,7 @@ class Homescreen extends StatefulWidget {
 
 class _HomescreenState extends State<Homescreen> {
   List<CommandsModel> commendsModel = [];
+
   getComments() {
     GetApiServices().getCommentsModel().then((value) {
       setState(() {
@@ -30,69 +31,90 @@ class _HomescreenState extends State<Homescreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.blueGrey.shade50,
         appBar: AppBar(
-          title: Center(child: Text("GET API DATA fetch")),
+          backgroundColor: Colors.blueGrey,
+          title: Center(child: Text("Get Data From API", 
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white
+            ))),
         ),
-        body: ListView.builder(
-          shrinkWrap: true,
-          itemCount: commendsModel.length,
-          itemBuilder: (context, index) {
-            final apidata = commendsModel[index];
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Material(
-                elevation: 5,
-                borderRadius: BorderRadius.circular(20),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      const SizedBox(
-                        width: 3,
+        body: commendsModel.isEmpty
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+              physics: BouncingScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: commendsModel.length,
+                itemBuilder: (context, index) {
+                  final apidata = commendsModel[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Card(
+                      elevation: 8,
+                      shadowColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
                       ),
-                      CircleAvatar(
-                        backgroundColor: Colors.black,
-                        child: Text(
-                          apidata.id.toString(),
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
-                              color: Colors.white),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width / 1.75,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Row(
                           children: [
-                            Text(
-                              apidata.name.toString(),
-                              maxLines: 1,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            CircleAvatar(
+                              radius: 30,
+                              backgroundColor: Colors.blueGrey.shade200,
+                              child: Text(
+                                apidata.id.toString(),
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                ),
+                              ),
                             ),
-                            Text(
-                              apidata.email.toString(),
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              apidata.body.toString(),
+                            const SizedBox(width: 20),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    apidata.name.toString(),
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                        color: Colors.black),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    apidata.email.toString(),
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 14,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    apidata.body.toString(),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.black87,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ],
                         ),
-                      )
-                    ],
-                  ),
-                ),
+                      ),
+                    ),
+                  );
+                },
               ),
-            );
-          },
-        ),
       ),
     );
   }
